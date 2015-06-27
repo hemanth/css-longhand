4744d36c73c8d5fe31d7df9aaa8b5aa51b728f49
Christoph Hermann <christoph.hermann@kaba.com>
2015-06-27 11:36:40 +0200

    make it easier to test the lib

--- test.js
+++ test.js
@@ -2,6 +2,8 @@
 var assert = require('assert');
 var cssLonghand = require('./');
 var data = require('fs').readFileSync('./fixture.css','utf-8');
+var expected = require('fs').readFileSync('./expected.css','utf-8');
+
 it('should expand css', function () {
-	assert.strictEqual(cssLonghand(data),'.classname {\n  margin-top: 0;\n  margin-right: 0;\n  margin-bottom: 0;\n  margin-left: 0;\n}\n');
+	assert.strictEqual(cssLonghand(data), expected);
 });
